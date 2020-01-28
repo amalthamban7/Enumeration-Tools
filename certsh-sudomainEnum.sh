5a377deba872a7b165d908df479ad9a358519da2
Amal E Thamban <32819491+amalthamban7@users.noreply.github.com>
2020-01-28 12:53:33 +0530

    Add files via upload
    certsh-subdomainEnum.sh used to enumerate domain from http://crt.sh/ and grepping domains only

---
+++ certsh-sudomainEnum.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+echo "Enter domain name:"
+read domain
+echo "Enter the name of output file with subdomains"
+read subdomains
+curl -s https://crt.sh/?q=%25.$domain | grep "$domain" | grep "<TD>" | cut -d">" -f2 | cut -d"<" -f1 | sort -u | sed s/*.//g >>$subdomains
+sort -u $subdomains
+done
